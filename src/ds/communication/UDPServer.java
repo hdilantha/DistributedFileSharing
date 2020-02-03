7bb1e5c7abd8f61d57d258cafe8f0f55ff5719dc
GunavaranBrihadiswaran <150196b@uom.lk>
2020-02-03 20:02:18 +0530

    UDPServer completed

--- src/ds/communication/UDPServer.java
+++ src/ds/communication/UDPServer.java
@@ -1,4 +1,41 @@
 package ds.communication;
 
-public class UDPServer {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.concurrent.BlockingQueue;
+
+public class UDPServer extends Thread{
+    private final BlockingQueue<ChannelMessage> channelIn;
+    private final DatagramSocket socket;
+    private volatile boolean process = true;
+    public UDPServer(BlockingQueue<ChannelMessage> channelIn, DatagramSocket socket) {
+        this.channelIn = channelIn;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        while (process) {
+
+            try {
+                byte[] response = new byte[65536];
+                DatagramPacket packet = new DatagramPacket(response, response.length);
+                socket.receive(packet);
+                String address = packet.getAddress().toString();
+                address = address.substring(1,address.length());
+                int port = packet.getPort();
+                String body = new String(response, 0, response.length);
+                ChannelMessage message = new ChannelMessage(address, port, body);
+                channelIn.put(message);
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        socket.close();
+    }
+
+    public void stopProcessing() {
+        this.process = false;
+    }
 }
